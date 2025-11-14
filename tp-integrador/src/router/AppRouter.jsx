d74a069ad5cacce018fb9a4a791ac8c37eb6df91
kiarajarzinski <jarzinskikiara@gmail.com>
2025-11-14 03:41:41 -0300

    app router

--- tp-integrador/src/router/AppRouter.jsx
+++ tp-integrador/src/router/AppRouter.jsx
@@ -1,10 +1,29 @@
-import { Routes,} from 'module'
+import { Login } from "../pages/Login.jsx";
+import { Register } from "../pages/Register.jsx";
+import { Profile } from "../pages/Profile.jsx";
+import { Home } from "../pages/Home.jsx";
+import { Tasks } from "../pages/Tasks.jsx";
+import { PrivateRoute } from "./PrivateRoute.jsx";
+import { PublicRoute } from "./PublicRoute.jsx"
+import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRouter = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Routes>
+      <Route element={<PublicRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Route>
 
+      <Route element={<PrivateRoute />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/home" />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+      </Route>
+    </Routes>
+  );
+};
